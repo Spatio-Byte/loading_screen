33a14edd55f0a482326af57825f28d6209160509
spatio <alex451.souchay@gmail.com>
2025-02-01 17:47:49 +0100

    site construit mais pas fini

---
+++ back-end/script.js
@@ -0,0 +1,13 @@
+const srvname = document.querySelector('#srv-name');
+
+
+window.onload = function() {
+    srvname.textContent = ""
+    
+    
+    let progress = 0;
+    const progressBar = document.querySelector('.progress');
+};
+
+
+document.getElementById("music").volume = 0.2;
